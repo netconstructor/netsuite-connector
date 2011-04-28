e93166ec8f1469d8de9ca7bd9b51a4e5f249657a
Franco Bulgarelli <franco@zaubersoftware.com>
2011-04-28 14:18:29 -0300

    Adding License Header

--- src/main/java/com/netsuite/webservices/transactions/general_2010_2/package-info.java
+++ src/main/java/com/netsuite/webservices/transactions/general_2010_2/package-info.java
@@ -1,2 +1,12 @@
+/**
+ * Mule NetSuite Cloud Connector
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 @javax.xml.bind.annotation.XmlSchema(namespace = "urn:general_2010_2.transactions.webservices.netsuite.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.netsuite.webservices.transactions.general_2010_2;
