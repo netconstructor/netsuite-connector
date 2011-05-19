ca41d3d4b6b6cbdbf53f490c0594a98a3c405a78
Franco Bulgarelli <franco@zaubersoftware.com>
2011-05-19 14:28:38 -0300

    Rewriting size and toString in PaginatedIterable
    Some mule components will eventually reimplement toString (!!)
    instead of invoking that message on a target collection.
    That implementation will also invoke size() (!!) , which was causing that
    when loggers were set to debug level, an UnsupportedOperationException was thrown.
    
    This is a design issue that requires more development in order to provide a better support
    for lazy iterables, instead of relaing on a hack - making such iterables extend AbstractCollection
    to work after instanceof tests - but, by now, I have reimplmented size so that it works,
    but produces a warn message indicating the innefficiency.

--- src/main/java/org/mule/module/netsuite/api/paging/PaginatedIterable.java
+++ src/main/java/org/mule/module/netsuite/api/paging/PaginatedIterable.java
@@ -8,22 +8,20 @@
  * LICENSE.txt file.
  */
 
-
 package org.mule.module.netsuite.api.paging;
 
 import java.util.AbstractCollection;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-/**
- * Warning: this class is not a proper collection, just it implements it in order to
- * be compatible with some mule's collection splitting
- * 
- * @author flbulgarelli
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class PaginatedIterable<T, Page> extends AbstractCollection<T> implements Iterable<T>
 {
+    private static Logger logger = LoggerFactory.getLogger(PaginatedIterable.class);
 
+    @Override
     public Iterator<T> iterator()
     {
         final Page initialPageInfo = firstPage();
@@ -97,6 +95,7 @@ public abstract class PaginatedIterable<T, Page> extends AbstractCollection<T> i
     @Override
     public Object[] toArray()
     {
+        warnEagerMessage("toArray");
         LinkedList<Object> l = new LinkedList<Object>();
         for (Object o : this)
         {
@@ -108,6 +107,37 @@ public abstract class PaginatedIterable<T, Page> extends AbstractCollection<T> i
     @Override
     public int size()
     {
-        throw new UnsupportedOperationException();
+        warnEagerMessage("size");
+        int i = 0;
+        for (@SuppressWarnings("unused")
+        Object o : this)
+        {
+            i++;
+        }
+        return i;
+    }
+    
+    /**
+     * Same impl that those found in Object, in order to avoid eager elements consumption
+     */
+    @Override
+    public String toString()
+    {
+        return getClass().getName() + "@" + Integer.toHexString(hashCode());
+    }
+
+    /**
+     * Warns that sending the given message implied processing all the elements, which is not efficient at all, 
+     * and most times is a bad idea, as lazy iterables should be traversed only once and in a lazy manner. 
+     * @param message
+     */
+    private static void warnEagerMessage(String message)
+    {
+        if (logger.isWarnEnabled())
+        {
+            logger.warn(
+                "Method {} needs to consume all the element. It is inefficient and thus should be used with care",
+                message);
+        }
     }
 }
